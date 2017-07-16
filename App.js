8479bba840d94adb3d99cd583012e54835d1aa35
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-07-16 02:42:43 -0300

    Pantalla inicial

--- App.js
+++ App.js
@@ -1,23 +1,113 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// @flow
+import React, {Component} from 'react';
+import Expo from 'expo';
+import {Dimensions, StyleSheet} from 'react-native';
+import {
+  Button,
+  Container,
+  Content,
+  Text,
+  Thumbnail,
+  View
+} from 'native-base';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+const {height: screenHeight} = Dimensions.get('window')
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  bgLight: {
+    backgroundColor: 'cornflowerblue'
   },
-});
+  bg: {
+    backgroundColor: 'steelblue'
+  },
+  bgDark: {
+    backgroundColor: 'darkslateblue'
+  },
+  fullScreen: {
+    flex: 1,
+    height: screenHeight
+  },
+  header: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: 'flex-end',
+    zIndex: 1
+  },
+  content: {
+    flex: 3
+  },
+  h1: {
+    fontSize: 32,
+    justifyContent: 'center',
+    marginTop: 60,
+    marginBottom: 15,
+    textAlign: 'center'
+  },
+  leadBlock: {
+    flex: 2,
+    textAlign: 'center'
+  },
+  lead:{
+    fontSize: 20,
+  },
+  textCenter: {
+    textAlign: 'center'
+  },
+  small: {
+    fontSize: 14,
+  },
+  valign: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
+
+let css = (styles) => {
+  return StyleSheet.flatten(styles)
+}
+
+export default class HelloWorld extends Component {
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({'Roboto': require('native-base/Fonts/Roboto.ttf'), 'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf')});
+  }
+
+  render() {
+    return (
+      <Container>
+        <Content>
+          <View style={css(styles.fullScreen)}>
+            <View style={css(styles.header)}>
+              <Thumbnail style={{
+                marginBottom: -40
+              }} large source={{
+                uri: 'http://gravatar.com/avatar/41b64c5cb658355a506deba471262427'
+              }}/>
+            </View>
+            <View style={css([styles.content])} padder>
+              <Text style={css(styles.h1)}>nahuelhds</Text>
+              <View style={css([{flex: 1}])}>
+                <View style={css(styles.leadBlock)}>
+                  <Text style={css([styles.lead, styles.textCenter])}>
+                    Agile developer & Craftperson
+                  </Text>
+                  <Text style={css([styles.lead, styles.textCenter])}>
+                    @ CristalMedia
+                  </Text>
+                </View>
+                <View style={css([styles.valign])}>
+                  <Text style={css([styles.textCenter, styles.small])}>Esta aplicación fue creada para conocer y aprender sobre React Native.</Text>
+                </View>
+              </View>
+              <View>
+                <Button full primary>
+                  <Text>Ingresar</Text>
+                </Button>
+              </View>
+            </View>
+          </View>
+        </Content>
+      </Container>
+    )
+  }
+}

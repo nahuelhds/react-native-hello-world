3e2f8d0d06b3c2d437380a42a0a992dd55e2168f
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-07-17 13:31:38 -0300

    Boilerplate to start with

--- src/Router.js
+++ src/Router.js
@@ -2,20 +2,20 @@ import React from "react";
 import {DrawerNavigator} from "react-navigation";
 
 import HomeScreen from "./HomeScreen/index";
-// import MainScreenNavigator from "./ChatScreen/index";
-// import Profile from "./ProfileScreen/index";
+import MainScreenNavigator from "./ChatScreen/index";
+import Profile from "./ProfileScreen/index";
 import SideBar from "./SideBar/index";
 
 const Router = DrawerNavigator({
   Home: {
     screen: HomeScreen
   },
-  // Chat: {
-  //   screen: MainScreenNavigator
-  // },
-  // Profile: {
-  //   screen: Profile
-  // }
+  Chat: {
+    screen: MainScreenNavigator
+  },
+  Profile: {
+    screen: Profile
+  }
 }, {
   contentComponent: props => <SideBar {...props}/>
 });

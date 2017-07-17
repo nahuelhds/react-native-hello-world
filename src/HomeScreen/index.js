7d5b643888a7f20379e5a0643387a163e62268bf
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-07-17 13:30:49 -0300

    React Navigation. Main entry point

---
+++ src/HomeScreen/index.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+import {
+  Button,
+  Container,
+  Content,
+  Text,
+  Thumbnail,
+  View
+} from 'native-base';
+
+import {styles, css} from './styles';
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <Container>
+        <Content>
+          <View style={css(styles.fullScreen)}>
+            <View style={css(styles.header)}>
+              <Thumbnail style={css(styles.avatar)} large source={{
+                uri: 'http://gravatar.com/avatar/41b64c5cb658355a506deba471262427'
+              }}/>
+            </View>
+            <View style={css([styles.content])} padder>
+              <Text style={css(styles.subtitle)}>The Hello World App</Text>
+              <Text style={css(styles.title)}>nahuelhds</Text>
+              <View style={css(styles.textContainer)}>
+                <View style={css(styles.leadBlock)}>
+                  <Text style={css([styles.lead, styles.textCenter])}>
+                    Agile developer & Craftperson
+                  </Text>
+                  <Text style={css([styles.lead, styles.textCenter])}>
+                    @ CristalMedia
+                  </Text>
+                </View>
+                <View style={css([styles.valign])}>
+                  <Text style={css([styles.textCenter, styles.small])}>Esta aplicación fue creada para conocer y aprender sobre React Native.</Text>
+                </View>
+              </View>
+              <View>
+                <Button full primary onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+                  <Text>Navegar</Text>
+                </Button>
+              </View>
+            </View>
+          </View>
+        </Content>
+      </Container>
+    )
+  }
+}
---
+++ src/HomeScreen/styles.js
@@ -0,0 +1,66 @@
+import {Dimensions, StyleSheet} from 'react-native';
+
+const {height: screenHeight} = Dimensions.get('window')
+const styles = StyleSheet.create({
+  bgLight: {
+    backgroundColor: 'cornflowerblue'
+  },
+  bg: {
+    backgroundColor: 'steelblue'
+  },
+  bgDark: {
+    backgroundColor: 'darkslateblue'
+  },
+  fullScreen: {
+    flex: 1,
+    height: screenHeight
+  },
+  header: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: 'flex-end',
+    zIndex: 1
+  },
+  avatar: {
+    marginBottom: -40
+  },
+  content: {
+    flex: 3
+  },
+  subtitle: {
+    fontSize: 16,
+    marginTop: 60,
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+  title: {
+    // fontFamily: 'Roboto',
+    fontSize: 32,
+    justifyContent: 'center',
+    marginBottom: 15,
+    textAlign: 'center'
+  },
+  textContainer: {
+    flex: 1
+  },
+  leadBlock: {
+    flex: 2
+  },
+  lead: {
+    fontSize: 20
+  },
+  textCenter: {
+    textAlign: 'center'
+  },
+  small: {
+    fontSize: 14
+  },
+  valign: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
+
+const css = StyleSheet.flatten
+
+export {styles, css}

ca56988b2b8e86e2817e90b3034f27138730572a
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-07-20 21:55:00 -0300

    eslint

--- src/AboutMeScreen/index.js
+++ src/AboutMeScreen/index.js
@@ -1,10 +1,34 @@
 import React from "react"
-import {TabNavigator} from "react-navigation"
-import {Button, Text, Icon, Footer, FooterTab} from "native-base"
+
+import {
+  TabNavigator
+} from "react-navigation"
+
+import {
+  Button,
+  Text,
+  Icon,
+  Footer,
+  FooterTab,
+} from "native-base"
 
 import Personal from "./Personal"
 // import CarreerScreen from "./CarreerScreen";
 // import SocialScreen from "./SocialScreen";
+
+const routes = [{
+  key: "Personal",
+  icon: "bowtie",
+  route: "Personal",
+}, {
+  key: "My Carreer",
+  icon: "bowtie",
+  route: "Carreer",
+}, {
+  key: "Social",
+  icon: "bowtie",
+  route: "Social",
+}]
 
 const AboutMeScreen = TabNavigator({
   Personal: {
@@ -15,21 +39,19 @@ const AboutMeScreen = TabNavigator({
   // },
   // NineChat: {
   //   screen: NineChat
-  // }
+  // },
 }, {
   tabBarPosition: "bottom",
-  tabBarComponent: props => {
-    return (
-      <Footer>
-        <FooterTab>
-          <Button vertical active={props.navigationState.index === 0} onPress={() => props.navigation.navigate("Personal")}>
-            <Icon name="bowtie"/>
-            <Text>Lucy</Text>
-          </Button>
-        </FooterTab>
-      </Footer>
-    );
-  }
+  tabBarComponent: props => (
+    <Footer>
+      <FooterTab>
+        {routes.map((item) => <Button key={item.key} vertical active={props.navigationState.index === 0} onPress={() => props.navigation.navigate(item.route)}>
+          <Icon name={item.icon}/>
+          <Text>{item.key}</Text>
+        </Button>)}
+      </FooterTab>
+    </Footer>
+  )
 })
 
 export default AboutMeScreen;

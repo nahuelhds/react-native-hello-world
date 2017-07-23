169102af91f79b41a9aac8ff6b941d39fceb1535
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-07-23 18:46:59 -0300

    ESLint

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
-import React, {Component} from "react";
-import Expo from "expo";
-import MainScreen from "./src/MainScreen/index";
+// @flow
+import React, { Component } from 'react';
+import Expo from 'expo';
+import MainScreen from './src/MainScreen/index';
 
 export default class TheHelloWordApp extends Component {
   constructor() {
     super();
     this.state = {
-      isReady: false
+      isReady: false,
     };
   }
   async componentWillMount() {
     const fonts = {
-      // 'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      // 'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      'Ionicons': require('native-base/Fonts/Ionicons.ttf')
-    }
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      Ionicons: require('native-base/Fonts/Ionicons.ttf'),
+    };
     await Expo.Font.loadAsync(fonts);
-    this.setState({isReady: true});
+    this.setState({ isReady: true });
   }
   render() {
     if (!this.state.isReady) {
-      return <Expo.AppLoading/>;
+      return <Expo.AppLoading />;
     }
-    return <MainScreen/>;
+    return <MainScreen />;
   }
 }
--- src/HomeScreen/index.js
+++ src/HomeScreen/index.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import {Button, Text, Thumbnail, View} from 'native-base';
+import React, { Component } from 'react';
+import { Button, Text, Thumbnail, View } from 'native-base';
 
-import {styles, css} from './styles';
+import { styles, css } from './styles';
 
 export default class HomeScreen extends Component {
   render() {
@@ -9,8 +9,8 @@ export default class HomeScreen extends Component {
       <View style={css(styles.fullHeight)}>
         <View style={css(styles.header)}>
           <Thumbnail style={css(styles.avatar)} large source={{
-            uri: 'http://gravatar.com/avatar/41b64c5cb658355a506deba471262427'
-          }}/>
+            uri: 'http://gravatar.com/avatar/41b64c5cb658355a506deba471262427',
+          }} />
         </View>
         <View style={css([styles.content])} padder>
           <Text style={css(styles.subtitle)}>The Hello World App</Text>
@@ -25,14 +25,16 @@ export default class HomeScreen extends Component {
               </Text>
             </View>
             <View style={css([styles.valign])}>
-              <Text style={css([styles.textCenter, styles.small])}>This app was developed for learning about React Native and its libraries</Text>
+              <Text style={css([styles.textCenter, styles.small])}>
+                This app was developed for learning about React Native and its libraries
+              </Text>
             </View>
           </View>
         </View>
-        <Button full primary onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+        <Button full primary onPress={() => this.props.navigation.navigate('DrawerOpen')}>
           <Text>Begin</Text>
         </Button>
       </View>
-    )
+    );
   }
 }

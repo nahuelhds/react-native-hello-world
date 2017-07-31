7da0e5bfbeb25f8ba160f76ff49121eacf5bca7d
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-07-31 08:05:35 -0300

    flowconfig. See https://github.com/facebook/flow/issues/869#issuecomment-256643823

---
+++ flow/libs.js
@@ -0,0 +1,8 @@
+// @flow
+declare module 'expo' { declare var exports: any; }
+declare module 'native-base' { declare var exports: any; }
+declare module 'native-base/Fonts/Ionicons.ttf' { declare var exports: any; }
+declare module 'native-base/Fonts/Roboto.ttf' { declare var exports: any; }
+declare module 'native-base/Fonts/Roboto_medium.ttf' { declare var exports: any; }
+declare module 'react-native' { declare var exports: any; }
+declare module 'react-navigation' { declare var exports: any; }

05a3bbb621fef52981cdf1537c428f77b8b0e1b0
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-07-31 09:25:00 -0300

    HomeScreen

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -8,14 +8,14 @@ import lang from 'lang/index'
 const screen = class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={css([styles.full, styles.vcenter])}>
-        <View style={css([styles.full, styles.hcenter, styles.vend])}>
+      <View style={css([styles.flex, styles.vcenter])}>
+        <View style={css([styles.flex, styles.hcenter, styles.vend])}>
           <Thumbnail large source={require("img/logo.png")} />
           <Text style={css(styles.heading)}>{lang.t('app.name')}</Text>
         </View>
-        <View style={css([styles.full, styles.vend])}>
+        <View style={css([styles.flex, styles.vend])}>
           <View padder>
-            <Text style={css(styles.textCenter)}>
+            <Text style={css([styles.textCenter, styles.small])}>
               {lang.t('homescreen.description')}
             </Text>
           </View>
@@ -30,7 +30,7 @@ const screen = class HomeScreen extends React.Component {
 
 const css = StyleSheet.flatten
 const styles = StyleSheet.create({
-  full: {
+  flex: {
     flex: 1,
   },
   hcenter:{
@@ -47,6 +47,9 @@ const styles = StyleSheet.create({
   },
   textCenter: {
     textAlign: 'center'
+  },
+  small: {
+    fontSize: 14
   }
 })
 

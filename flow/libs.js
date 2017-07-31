2a80808586958c6d2069214743d02abc5e20d99f
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-07-31 09:18:52 -0300

    ESLint & Flow. Assets integration

--- flow/libs.js
+++ flow/libs.js
@@ -1,8 +1,6 @@
 // @flow
 declare module 'expo' { declare var exports: any; }
+declare module 'img' { declare var exports: any; }
 declare module 'native-base' { declare var exports: any; }
-declare module 'native-base/Fonts/Ionicons.ttf' { declare var exports: any; }
-declare module 'native-base/Fonts/Roboto.ttf' { declare var exports: any; }
-declare module 'native-base/Fonts/Roboto_medium.ttf' { declare var exports: any; }
 declare module 'react-native' { declare var exports: any; }
 declare module 'react-navigation' { declare var exports: any; }

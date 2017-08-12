70b5463787b228b57c37afa85b067cd76da29638
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-08-11 21:42:13 -0300

    Elaboración de comportamiento de ir al siguiente campo al terminar de completar uno (tabindex en web). Particularmente para NativeBase, ver: https://github.com/GeekyAnts/NativeBase/issues/70#issuecomment-288434297

--- src/screen/ContactMe.js
+++ src/screen/ContactMe.js
@@ -27,7 +27,7 @@ export default class ContactMeScreen extends React.Component {
         onPress={() => this.props.navigation.navigate("DrawerOpen")}
       ></DrawerHeader>
       <Content>
-        <View style={{alignItems:"center"}}>
+        <View style={{ alignItems: "center" }}>
           <Icon active name="help-buoy" style={{ fontSize: 100, color: Theme.brandInfo }}></Icon>
           <Text style={{ textAlign: 'center' }}>{lang.t('contactme.description')}</Text>
           <H1>{lang.t('contactme.title')}</H1>
@@ -36,15 +36,28 @@ export default class ContactMeScreen extends React.Component {
           <Form>
             <Item floatingLabel>
               <Label>{lang.t('form.label.name')}</Label>
-              <Input/>
+              <Input
+                autoFocus="true"
+                getRef={(component) => { this.textInput = component; }}
+                onSubmitEditing={() => { this.emailInput._root.focus(); }}
+              />
             </Item>
             <Item floatingLabel>
               <Label>{lang.t('form.label.email')}</Label>
-              <Input/>
+              <Input
+                keyboardType="email-address"
+                autoCapitalize="none"
+                getRef={(component) => { this.emailInput = component; }}
+                onSubmitEditing={() => { this.messageInput._root.focus(); }}
+              />
             </Item>
             <Item floatingLabel>
               <Label>{lang.t('form.label.message')}</Label>
-              <Input multiline={true} style={css(styles.textArea)} />
+              <Input
+                multiline={true}
+                style={css(styles.textArea)}
+                getRef={(component) => { this.messageInput = component; }}
+              />
             </Item>
           </Form>
         </View>

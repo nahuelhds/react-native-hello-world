5398d2196781487796fed6842c171581834e0426
Nahuel Sotelo <nsotelo@cristalmedia.net>
2017-09-09 22:54:42 -0300

    Envío de mail mediante API de Mailgun y uso de Frisbee

--- src/service/EmailClient.js
+++ src/service/EmailClient.js
@@ -1,29 +1,55 @@
 // @flow
-import Mailgun from 'mailgun.js'
+import Frisbee from 'frisbee'
+import lang from 'lang/main'
 
 const API_KEY: String = "key-740bf9828d2bf1134090e6b3f5cd2f74";
 const DOMAIN: String = "sandbox965964606d6d4762bd42d5e5d187e425.mailgun.org";
 
-export type EmailMessage = {
-  from: String,
-  to: Array,
-  subject: String,
+export type EmailUserData = {
+  email: String,
+  name: String,
   message: String,
 }
 
+type EmailMessage = {
+  from: String,
+  to: Array<String>,
+  subject: String,
+  html: String,
+  text?: String,
+}
+
 class EmailClient {
-  client: *;
+  api: *;
 
   constructor() {
-    this.client = Mailgun.client({
-      username: 'api',
-      key: API_KEY
-    })
+    this.api = new Frisbee({
+      baseURI: `https://api.mailgun.net/v3/${DOMAIN}`,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      auth: ['api', API_KEY],
+    });
   }
 
-  message(message: EmailMessage) {
-    this.client.messages.create(DOMAIN, message)
+  message(data: EmailUserData) {
+    const message: EmailMessage = {
+      from: `React Native Hello World <mailgun@${DOMAIN}>`,
+      to: "nsotelo@cristalmedia.net",
+      subject: lang.t('email.subject'),
+      html: '<h2>' + lang.t('email.title', data) + '</h2><p>' + lang.t('email.description', data) + '</p>',
+      text: lang.t('email.title', data) + "\n" + lang.t('email.description', data),
+    }
+
+    return this.api.post('/messages', { body: encodeData(message) })
   }
+}
+
+function encodeData(json) {
+  return Object.keys(json).map(function (key) {
+    return encodeURIComponent(key) + '=' +
+      encodeURIComponent(json[key]);
+  }).join('&');
 }
 
 const emailClient = new EmailClient
